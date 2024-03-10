f44185d6af57ebbc6ebf25838e6752ae567c806e
Flo <flo@uwap.org>
2024-03-10 04:08:32 +0100

    Added event Action? TrackOver.

---
+++ Events.cs
@@ -0,0 +1,9 @@
+﻿namespace uwap.AudioPlayerLibrary;
+
+public static partial class AudioPlayer
+{
+    /// <summary>
+    /// This event is called when the currently playing audio track is over (but not when Stop() is called).
+    /// </summary>
+    public static event Action? TrackOver = null;
+}
