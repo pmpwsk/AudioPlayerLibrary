66991f3d65047044f760b218108d989b1dc91136
Flo <flo@uwap.org>
2024-03-10 04:06:55 +0100

    Added bool KeepOpen.

---
+++ Config.cs
@@ -0,0 +1,12 @@
+﻿using ManagedBass;
+
+namespace uwap.AudioPlayerLibrary;
+
+public static partial class AudioPlayer
+{
+    /// <summary>
+    /// Whether to keep the audio engine initialized after playback has stopped.<br/>
+    /// Default: false
+    /// </summary>
+    public static bool KeepOpen { get; set; } = false;
+}
