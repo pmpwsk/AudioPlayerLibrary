87eaf20134da4ce78fb12864645bb0ac80e20eab
Flo <flo@uwap.org>
2024-03-10 03:59:19 +0100

    Added enum States.

---
+++ State.cs
@@ -0,0 +1,12 @@
+namespace uwap.AudioPlayerLibrary;
+
+public static partial class AudioPlayer
+{
+    public enum States
+    {
+        Uninitialized,
+        Stopped,
+        Paused,
+        Playing
+    }
+}
