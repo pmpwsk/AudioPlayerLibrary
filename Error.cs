40d1365f3bdfa4c18aa0277f0838d44312450aca
Flo <flo@uwap.org>
2024-03-10 04:05:53 +0100

    Added Exception DetailedException().

--- Error.cs
+++ Error.cs
@@ -4,6 +4,9 @@ namespace uwap.AudioPlayerLibrary;
 
 public static partial class AudioPlayer
 {
+    private static Exception DetailedException()
+        => new(DetailedError());
+
     private static string DetailedError()
         => Bass.LastError switch
         {
