0519a294cc3b94d65be679d3595a8ffd1cd7dd08
Flo <flo@uwap.org>
2024-03-10 04:05:33 +0100

    Added string DetailedError().

---
+++ Error.cs
@@ -0,0 +1,15 @@
+using ManagedBass;
+
+namespace uwap.AudioPlayerLibrary;
+
+public static partial class AudioPlayer
+{
+    private static string DetailedError()
+        => Bass.LastError switch
+        {
+            Errors.Busy => "The audio driver is busy, most likely because something else is playing and the driver doesn't support multiple audio streams at once.",
+            Errors.Unknown => "An unknown error occurred.",
+            Errors.OK => "No error.",
+            _ => $"Unrecognized error \"{Bass.LastError}\"."
+        };
+}
