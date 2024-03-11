c6c6cf1724ba81b4beced517ec21695aa910c0a3
Flo <flo@uwap.org>
2024-03-11 04:05:14 +0100

    Added Close().

--- Init.cs
+++ Init.cs
@@ -24,4 +24,28 @@ public static partial class AudioPlayer
             throw new Exception("Failed to open the audio engine: " + (ex.Message.Contains("Unable to load shared library 'bass'") ? "The BASS library could not be loaded, most likely because bass.dll (Windows) or libbass.so (Linux) is not in this program's directory. Refer to https://uwap.org/no-bass in order to resolve this issue." : ex.Message));
         }
     }
+
+    /// <summary>
+    /// Manually closes the audio engine.<br/>
+    /// If something is playing, playback will be stopped first.
+    /// </summary>
+    public static void Close()
+    {
+        try
+        {
+            if (State == States.Uninitialized)
+                return;
+            if (State != States.Stopped)
+                Stop(false);
+
+            if (!Bass.Free())
+                throw DetailedException();
+
+            State = States.Uninitialized;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Failed to close the audio engine: " + ex.Message);
+        }
+    }
 }
