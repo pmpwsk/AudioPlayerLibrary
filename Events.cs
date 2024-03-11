e6fd2ad3b69a9dcc6141af7cd486247b24dd16b8
Flo <flo@uwap.org>
2024-03-11 04:07:44 +0100

    Added TrackOverSync(...).

--- Events.cs
+++ Events.cs
@@ -6,4 +6,10 @@ public static partial class AudioPlayer
     /// This event is called when the currently playing audio track is over (but not when Stop() is called).
     /// </summary>
     public static event Action? TrackOver = null;
+
+    private static void TrackOverSync(int Handle, int Channel, int Data, IntPtr User)
+    {
+        Stop();
+        TrackOver?.Invoke();
+    }
 }
